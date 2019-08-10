a0c45ce5d973591965e3373d6c0b19575e5e2f17
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-10 15:59:27 -0400

    changed to capital

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header.jsx';
-import Portfolio from './portfolio/portfolio.jsx';
+import Portfolio from './portfolio/Portfolio.jsx';
 
 const Main = () => {
   return ( 
