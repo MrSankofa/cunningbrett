42079ec27bed9368bf0dd999ae691f9b66c4b471
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-10 14:32:54 -0400

    may be issue with file name

--- client/src/components/frame.jsx
+++ client/src/components/frame.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Header from '../components/Header.jsx';
 import Portfolio from './portfolio/Portfolio.jsx';
 
-export default class Frame extends React.Component {
-  render() {
-    return <React.Fragment>
+// export default class Frame extends React.Component {
+//   render() {
+//     return <React.Fragment>
+//       <Header/>
+//       <Portfolio/>
+//     </React.Fragment>
+//   }
+// }
+
+const Frame = () => {
+  return (
+    <React.Fragment>
       <Header/>
       <Portfolio/>
     </React.Fragment>
-  }
+  )
 }
+
+export default Frame;
