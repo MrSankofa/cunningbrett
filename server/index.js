94dc918ca6882edb640061ad56521ad75345c773
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-09 20:51:48 -0400

    created foundation

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const parser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(parser.json())
+
+app.use(express.static(__dirname + '/../client/dist'))
+
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('ERROR connecting to server:', err)
+  }
+  console.log(`cunningbrett is listening on port ${PORT}!`)
+})
