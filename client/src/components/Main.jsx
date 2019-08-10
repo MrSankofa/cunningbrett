501ac406140f5f960eab8c8f88c8a9787f944926
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-10 15:36:12 -0400

    it does not like directory name changes

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header.jsx';
-import Portfolio from './portfolio/Portfolio.jsx';
+import Portfolio from './portfolio/portfolio.jsx';
 
 const Main = () => {
   return ( 
