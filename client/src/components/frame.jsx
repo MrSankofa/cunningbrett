6278b48211d1fdfe0091aa1c79eb29cc3d99ad8b
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-10 14:02:13 -0400

    create React Apps sections

--- client/src/components/frame.jsx
+++ client/src/components/frame.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
-import About from '../components/About.jsx';
+import Portfolio from './portfolio/Portfolio.jsx';
 
 export default class Frame extends React.Component {
   render() {
     return <React.Fragment>
       <Header/>
-      <About/>
+      <Portfolio/>
     </React.Fragment>
   }
 }
---
+++ client/src/components/portfolio/react/YoutubeApp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const YoutubeApp = () => {
+  return (
+    <React.Fragment>
+      <h2><span><em>React.js (click images)</em></span></h2>
+      <section className="portfolio" >
+        <section className="thirds">
+          <a href="recastlyRedux.html">
+            <img src="img/reactApp.jpg" atl="" id="registry-img"/>
+            <p id="para-target">YouTube API React Redux App</p>
+          </a>
+
+        </section>
+      </section>
+    </React.Fragment>
+  )
+}
+
+export default YoutubeApp
